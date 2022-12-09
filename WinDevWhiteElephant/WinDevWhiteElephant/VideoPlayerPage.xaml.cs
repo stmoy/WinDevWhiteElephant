bda4340844eb9f9b627ee9c7f2ed9c5a650c3816
Steven Moyes <stmoy@microsoft.com>
2022-12-09 14:07:34 -0800

    Add video player page with a few videos

---
+++ WinDevWhiteElephant/WinDevWhiteElephant/VideoPlayerPage.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Windows.Data.Html;
+using Windows.UI.Xaml.Controls;
+
+using static System.Net.Http.HttpClientHandler;
+using HtmlAgilityPack;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace WinDevWhiteElephant
+{
+    public sealed partial class VideoPlayerPage : Page
+    {
+        ObservableCollection<Video> Videos;     
+
+        public VideoPlayerPage()
+        {
+            this.InitializeComponent();
+            
+            PopulateVideos();
+
+            VideosList.ItemsSource = Videos;
+        }
+
+        public void PopulateVideos()
+        {
+            Videos = new ObservableCollection<Video>();
+
+            Videos.Add(new Video() { URL = "https://youtu.be/cX9HrnX602M?t=18" });
+            Videos.Add(new Video() { URL = "https://youtu.be/QGBdwZNAqIQ?t=20" });
+        }
+
+        private void VideosList_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            VideoPlayer.Source = new Uri((e.ClickedItem as Video).URL);
+
+        }
+    }
+
+    public sealed class Video : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string status)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(status));
+        }
+
+        private HttpClient client = new HttpClient();
+
+        private string _title;
+        public String Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+
+        private String _url;
+
+        public String URL
+        {
+            get { return _url; }
+            set
+            {
+                _url = value;
+                GetTitleFromURL(value);
+            }
+        }
+
+        private async void GetTitleFromURL(string url)
+        {
+            string html = await client.GetStringAsync(url);
+
+            var htmlDocument = new HtmlAgilityPack.HtmlDocument();
+            htmlDocument.LoadHtml(html);
+            string title = htmlDocument.DocumentNode.SelectSingleNode("//title").InnerText;
+
+            Title = title;
+        }
+
+        public Video()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
