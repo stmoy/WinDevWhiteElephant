9a7762b94d25222510e1dddd314297565104b1d8
Steven Moyes <stmoy@microsoft.com>
2022-12-09 15:04:53 -0800

    Add TabView to swap between game and video

---
+++ WinDevWhiteElephant/WinDevWhiteElephant/RootPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls;
+using MUX = Microsoft.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace WinDevWhiteElephant
+{
+    public sealed partial class RootPage : Page
+    {
+        public RootPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            TabViewItem MainPageTab = new TabViewItem()
+            {
+                IconSource = new MUX.SymbolIconSource { Symbol = Symbol.ViewAll },
+                Header = "Game",
+                DataContext = typeof(MainPage),
+                Content = new Frame(),
+                IsClosable = false
+            };
+
+            TabViewItem VideoPlayerTab = new TabViewItem()
+            {
+                IconSource = new MUX.SymbolIconSource { Symbol = Symbol.Video },
+                Header = "Videos",
+                DataContext = typeof(VideoPlayerPage),
+                Content = new Frame(),
+                IsClosable = false
+            };
+
+            // Pre-load the content from both pages
+            (MainPageTab.Content as Frame).Navigate(MainPageTab.DataContext as System.Type);
+            (VideoPlayerTab.Content as Frame).Navigate(VideoPlayerTab.DataContext as System.Type);
+
+            RootTabView.TabItems.Add(MainPageTab);
+            RootTabView.TabItems.Add(VideoPlayerTab);
+
+            RootTabView.SelectedIndex = 0;
+        }
+
+    }
+}
